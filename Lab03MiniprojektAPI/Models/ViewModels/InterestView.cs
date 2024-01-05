19984106e092723c720851ad8b548d6bece6db8e
SurrealEmil <emil.ejderklev1@hotmail.com>
2024-01-05 16:11:47 +0100

    View models
    PersonView
    InterestView
    WebsiteLinkView

---
+++ Lab03MiniprojektAPI/Models/ViewModels/InterestView.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab03MiniprojektAPI.Models.ViewModels
+{
+    public class InterestView
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Lab03MiniprojektAPI/Models/ViewModels/PersonView.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab03MiniprojektAPI.Models.ViewModels
+{
+    public class PersonView
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public virtual ICollection<InterestView> Interests { get; set; }
+        public virtual ICollection<WebsiteLinkView> WebsiteLinks { get; set; }
+    }
+}
